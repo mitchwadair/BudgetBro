58f10494fcdf878a1874c42549fdeab196d69e55
mitchwadair <mitchwadair@gmail.com>
2019-01-29 18:20:32 -0500

    adds file i/o error handling

--- src/functionality/data_io.py
+++ src/functionality/data_io.py
@@ -2,19 +2,29 @@ import json
 
 
 def get_user_budget_settings():
-    user_data_file = open("../../data/user_data.txt", "r")
-    user_data = json.loads(user_data_file.read())
-    user_data_file.close()
-    return user_data
+    try:
+        user_data_file = open("../../data/user_data.txt", "r")
+    except FileNotFoundError:
+        print("User data file not found.")
+        return None
+    else:
+        user_data = json.loads(user_data_file.read())
+        user_data_file.close()
+        return user_data
 
 
 def get_user_expenses_data():
-    user_expenses_file = open("../../data/user_expenses.txt", "r")
-    user_expenses = json.loads(user_expenses_file.read())
-    user_expenses_file.close()
-    return user_expenses
+    try:
+        user_expenses_file = open("../../data/user_expenses.txt", "r")
+    except FileNotFoundError:
+        print("User expenses file not found.")
+        return None
+    else:
+        user_expenses = json.loads(user_expenses_file.read())
+        user_expenses_file.close()
+        return user_expenses
 
 
 #test that file i/o is working
-#print(get_user_budget_settings()["salary"])
+#print(get_user_budget_settings())
 #print(get_user_expenses_data())
