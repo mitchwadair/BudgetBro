d6c306c19f5f466fe68f7bffe8d04a762e8e3730
mitchwadair <mitchwadair@gmail.com>
2019-01-29 20:30:15 -0500

    changes expense structure to have year and month category rather than an entire date

--- src/functionality/app.py
+++ src/functionality/app.py
@@ -1,5 +1,4 @@
 import data_io
-import json
 
 user_expense_data = None
 user_budget_data = None
@@ -16,25 +15,22 @@ def display_home_view():
     print("This will display the home view")
 
 
-def add_new_expense(category, date, location, amount):
+def add_new_expense(category, year, month, location, amount):
     if category in user_expense_data["categories"]:
         # add data to existing category
-        if date in user_expense_data["categories"][category]:
-            entries = len(user_expense_data["categories"][category][date])
-            print(entries)
-            user_expense_data["categories"][category][date][entries + 1] = {"location": location, "amount": amount}
+        if year in user_expense_data["categories"][category]:
+            if month in user_expense_data["categories"][category][year]:
+                entries = len(user_expense_data["categories"][category][year][month])
+                # print(entries)
+                user_expense_data["categories"][category][year][month][entries + 1] = {"location": location, "amount": amount}
+            else:
+                user_expense_data["categories"][category][year][month] = {1: {"location": location, "amount": amount}}
         else:
-            user_expense_data["categories"][category][date] = {1: {"location": location, "amount": amount}}
+            user_expense_data["categories"][category][year] = {month: {1: {"location": location, "amount": amount}}}
     else:
         # create new category and add data to it
-        user_expense_data["categories"][category] = {date: {1: {"location": location, "amount": amount}}}
+        user_expense_data["categories"][category] = {year: {month: {1: {"location": location, "amount": amount}}}}
 
 
 setup()
 display_home_view()
-
-add_new_expense("gas", "1-29-2019", "BP", 3.66)
-add_new_expense("new", "1-29-2019", "place", 4.20)
-add_new_expense("gas", "1-28-2019", "Shell", 40.69)
-
-data_io.store_data("../../data/user_expenses.json", user_expense_data)
